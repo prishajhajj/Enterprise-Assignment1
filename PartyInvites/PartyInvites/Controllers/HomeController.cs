7c6ec8e449436260a93e106572b58e8e09b1c194
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-10-02 10:30:47 -0400

    Changed the index method of the HomeController.cs to render a view.

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -6,9 +6,9 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            return View();
         }
     }
 }
