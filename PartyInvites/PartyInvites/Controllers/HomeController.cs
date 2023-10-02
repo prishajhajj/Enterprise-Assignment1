b41ca4a81322b9a7bc696904ef54521a578bed94
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-10-02 17:25:05 -0400

    Added the PartyInvites.Models using statement. Aswell as added [HttpGet] above the Index method and added an action method RsvpForm to support POST requests.

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 using System.Diagnostics;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
-        public ViewResult RsvpForm()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View();
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
         }
     }
 }
