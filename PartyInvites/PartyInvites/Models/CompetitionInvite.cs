0c6b153df4bfc7e03736394a3cff324c029b3a95
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-10-02 21:46:47 -0400

    Added a new model class called CompetitionInvite.cs

---
+++ PartyInvites/PartyInvites/Models/CompetitionInvite.cs
@@ -0,0 +1,13 @@
+﻿using PartyInvites.Enums;
+
+namespace PartyInvites.Models
+{
+    public class CompetitionInvite
+    {
+        public string Address { get; set; }
+
+        public string TwitterAccount { get; set; }
+
+        public TechnicalInterest Interest { get; set; }
+    }
+}
